8a45fbd8017fac1a913a3d5ccc589d659ecf4fcc
srimani-programmer <srimani.scholar@gmail.com>
2020-03-30 23:18:44 +0530

    Initial Commit.

---
+++ calc.py
@@ -0,0 +1,22 @@
+def add(a,b):
+	return (a + b)
+
+def sub(a,b):
+	return a - b
+
+def multipy(a,b):
+	return a * b
+
+def divide(a,b):
+	return a/b
+
+def modulus(a,b):
+	return a%b
+
+
+if __name__ == "__main__":
+	print(add(3,5))
+	print(sub(5,3))
+	print(multipy(5,3))
+	print(divide(5,3))
+	print(modulus(2,4))
